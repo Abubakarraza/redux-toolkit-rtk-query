179ed4013adfb0dd40e9bd012d2874e3eae8ea1b
M Abubakar Raza <abubakarraza64@gmail.com>
2023-05-07 10:56:23 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { ContactList } from "./components/ContactList";
-import { ProductList } from "./components/ProductList";
 import { Routing } from "./routing/Routing";
 
 function App() {
